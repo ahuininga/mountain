5622923de2c12803f69d4434fa1a20c8bb034425
Alida <alida.huininga@eezeebee.com>
2020-06-15 22:49:47 +0200

    Fix some more issues

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware groups.
      *
-     * @var array<string,array<int, class-string>>
+     * @var array<string,array<int, string>>
      */
     protected $middlewareGroups = [
         'web' => [
