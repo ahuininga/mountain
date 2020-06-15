606b5cf2b2ea210923efa5a8bb7235c1027a8ea6
Alida <alida.huininga@eezeebee.com>
2020-06-15 22:16:09 +0200

    Fix some issues

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,7 +12,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event listener mappings for the application.
      *
-     * @var array
+     * @var array<event,eventListener>
      */
     protected $listen = [
         Registered::class => [
