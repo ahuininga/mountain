c2dc26ad053766e8c8992049355e5d60ae18131a
Huininga <ahuininga@users.noreply.github.com>
2024-07-04 14:34:14 +0200

    Fix migrations

--- database/migrations/2024_07_02_141618_create_team_invitations_table.php
+++ database/migrations/2024_07_02_141618_create_team_invitations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('team_invitations', function (Blueprint $table) {
-            $table->Uuid('id')->primary();
+            $table->uuid('id')->primary();
             $table->foreignUuid('team_id')->constrained()->cascadeOnDelete();
             $table->string('email');
             $table->string('role')->nullable();
